dcabd3227e068a233685ca956fb2c5ac8b32056b
Alobondo <45467026+Alobondo@users.noreply.github.com>
2021-12-17 17:50:29 -0300

    Add files via upload

---
+++ R/example_2_params.R
@@ -0,0 +1,19 @@
+#' Frequency analysis result for 2 parameters distributions, with first column as observations.
+#'
+#' A dataset containing result of a frequency analysis for 2 parameters distributions.
+#'
+#' @format A data frame with 48 rows and 11 variables:
+#' \describe{
+#' \item{Obs}{observations}
+#' \item{weibull}{weibull}
+#' \item{norm}{normal}
+#' \item{lnorm}{log normal}
+#' \item{gamma}{2 parameters gamma}
+#' \item{gumbel}{gumbel}
+#' \item{logis}{logistic}
+#' \item{cauchy}{cauchy}
+#' \item{llogis}{log logistic}
+#' \item{pareto}{pareto}
+#' \item{trgamma}{transformed gamma}
+#' }
+"example_2_params"
---
+++ R/example_3_params.R
@@ -0,0 +1,13 @@
+#' Frequency analysis result for 3 parameters distributions, with first column as observations.
+#'
+#' A dataset containing result of a frequency analysis for 3 parameters distributions.
+#'
+#' @format A data frame with 48 rows and 5 variables:
+#' \describe{
+#' \item{Obs}{observations}
+#' \item{gamma3}{3 parameters gamma}
+#' \item{gev}{gev}
+#' \item{lgamma3}{3 parameters log gamma}
+#' \item{lnorm3}{3 parameters log normal}
+#' }
+"example_3_params"
